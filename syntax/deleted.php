518fc734b6adae51d149ab42629ac17ae31565df
Michael Klier <chi@chimeric.de>
2009-02-06 20:39:32 +0100

    use correct bbcode sytnax
    darcs-hash:20090206193932-23886-b0380c9131e7c0d4f34164a19536f449aed9f45e.gz

--- syntax/deleted.php
+++ syntax/deleted.php
@@ -24,7 +24,7 @@ class syntax_plugin_bbcode_deleted extends DokuWiki_Syntax_Plugin {
             'author' => 'Gina Häußge, Michael Klier, Esther Brunner',
             'email'  => 'dokuwiki@chimeric.de',
             'date'   => @file_get_contents(DOKU_PLUGIN.'bbcode/VERSION'),
-            'name'   => 'BBCode Italic Plugin',
+            'name'   => 'BBCode Deleted Plugin',
             'desc'   => 'allows BBCode markup: [d]text[/d]',
             'url'    => 'http://wiki.splitbrain.org/plugin:bbcode',
         );
@@ -33,8 +33,8 @@ class syntax_plugin_bbcode_deleted extends DokuWiki_Syntax_Plugin {
     function getType() { return 'formatting'; }
     function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }   
     function getSort() { return 105; }
-    function connectTo($mode) { $this->Lexer->addEntryPattern('\[d\](?=.*?\x5B/d\x5D)',$mode,'deleted'); }
-    function postConnect() { $this->Lexer->addExitPattern('\[/d\]','deleted'); }
+    function connectTo($mode) { $this->Lexer->addEntryPattern('\[s\](?=.*?\x5B/s\x5D)',$mode,'deleted'); }
+    function postConnect() { $this->Lexer->addExitPattern('\[/s\]','deleted'); }
 
     /**
      * Handle the match
