5f99b58152b30adf66faff56ff6f1e8243bfee06
Michael Klier <chi@chimeric.de>
2009-02-06 20:34:17 +0100

    added deleted mode
    darcs-hash:20090206193417-23886-3b1405053a02a9b371535eee499b952545da36bb.gz

---
+++ syntax/deleted.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * BBCode plugin: allows BBCode markup familiar from forum software
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Esther Brunner <esther@kaffeehaus.ch>
+ */
+ 
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+ 
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_bbcode_deleted extends DokuWiki_Syntax_Plugin {
+ 
+    /**
+     * return some info
+     */
+    function getInfo() {
+        return array(
+            'author' => 'Gina Häußge, Michael Klier, Esther Brunner',
+            'email'  => 'dokuwiki@chimeric.de',
+            'date'   => @file_get_contents(DOKU_PLUGIN.'bbcode/VERSION'),
+            'name'   => 'BBCode Italic Plugin',
+            'desc'   => 'allows BBCode markup: [d]text[/d]',
+            'url'    => 'http://wiki.splitbrain.org/plugin:bbcode',
+        );
+    }
+ 
+    function getType() { return 'formatting'; }
+    function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }   
+    function getSort() { return 105; }
+    function connectTo($mode) { $this->Lexer->addEntryPattern('\[d\](?=.*?\x5B/d\x5D)',$mode,'deleted'); }
+    function postConnect() { $this->Lexer->addExitPattern('\[/d\]','deleted'); }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler) {
+        return array();
+    }
+ 
+    /**
+     * Create output
+     */
+    function render($mode, &$renderer, $data) {
+        return true;
+    }
+}
+// vim:ts=4:sw=4:et:enc=utf-8:     
